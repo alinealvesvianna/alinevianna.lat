bebbe4debded6496a34c352070aaf93d288a8f36
alinealvesvianna <alinealvesvianna@gmail.com>
2024-09-26 00:41:38 +0000

    initial files

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+http.createServer(function (req, res) {
+res.write("on way to being a full stack engineer!");
+res.end();
+
+
+}).listen(3000)
+
+console.log("server started on port 3000")
